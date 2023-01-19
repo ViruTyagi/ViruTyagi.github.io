9152f4bc4883d672ec4f8e3e22a7caf72d51d2eb
Vishavesh Tyagi <virutyagi9@gmail.com>
2023-01-20 02:30:31 +0530

    New Begenning

---
+++ script.js
@@ -0,0 +1,8 @@
+// Get the button element
+var ctaButton = document.querySelector(".cta-button");
+
+// Add a click event listener to the button
+ctaButton.addEventListener("click", function() {
+  // Show an alert message when the button is clicked
+  alert("Thank you for signing up!");
+})
